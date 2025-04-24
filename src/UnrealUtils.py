154d870667431ba3cbc0bd229cfaa8ebc457d515
Gabriel Flores <116604602+gflores328@users.noreply.github.com>
2025-04-24 16:16:32 -0500

    added UnrealUtils

--- src/UnrealUtils.py
+++ src/UnrealUtils.py
@@ -1,1 +1,25 @@
-print ("Hello from VS Code")
+import os
+import unreal
+
+def ImportMeshAndAnimations(meshPath, animDir):
+    importTask = unreal.AssetImportTask()
+    importTask.filename = meshPath
+
+    fileName = os.path.basename(meshPath).split('.')[0]
+    importTask.destination_path = '/Game/' + fileName
+
+    importTask.automated = True
+    importTask.save = True
+    importTask.replace_existing = True
+
+    importOption = unreal.FbxImportUI()
+    importOption.import_mesh = True
+    importOption.import_as_skeletal = True
+    importOption.skeletal_mesh_import_data.set_editor_property('import_morph_targets', True)
+    importOption.skeletal_mesh_import_data.set_editor_property('use_t0_as_ref_pose', True)
+
+    importTask.options = importOption
+
+    unreal.AssetToolsHelpers.get_asset_tools().import_asset_tasks([importTask])
+
+ImportMeshAndAnimations()
